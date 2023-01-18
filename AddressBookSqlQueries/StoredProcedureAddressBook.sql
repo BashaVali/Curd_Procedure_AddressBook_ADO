d087755cbf404cf7713c741530d85f0e153e9310
BashaVali <bashavali222@gmail.com>
2023-01-18 07:03:26 +0530

    Address Book Ado Retrive Entries

---
+++ AddressBookSqlQueries/StoredProcedureAddressBook.sql
@@ -0,0 +1,38 @@
+--Adding new AddressBook
+CREATE PROCEDURE SPAddingNewData(
+@FirstName VARCHAR(30),
+@LastName VARCHAR(30),
+@Address VARCHAR(100),
+@City VARCHAR(20),
+@State VARCHAR(20),
+@ZIP INT,
+@PhoneNumber BIGINT,
+@Email VARCHAR(50)
+)
+As
+Begin Try
+INSERT INTO AddressBookADO(FirstName,LastName,Address,City,State,ZIP,PhoneNumber,Email)
+VALUES(@FirstName,@LastName,@Address,@City,@State,@ZIP,@PhoneNumber,@Email)
+End Try
+Begin Catch
+SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_STATE() AS ErrorState,
+    ERROR_PROCEDURE() AS ErrorProcedure,
+    ERROR_LINE() AS ErrorLine,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+--Retrieving All Details From AddressBook
+CREATE PROCEDURE SPRetrieveAllDetails
+As
+Begin Try
+SELECT * FROM AddressBookADO
+End Try
+Begin Catch
+SELECT
+    ERROR_NUMBER() AS ErrorNumber,
+    ERROR_STATE() AS ErrorState,
+    ERROR_PROCEDURE() AS ErrorProcedure,
+    ERROR_LINE() AS ErrorLine,
+    ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
--- Curd_Procedure_AddressBook_ADO/AddressBook.cs
+++ Curd_Procedure_AddressBook_ADO/AddressBook.cs
@@ -8,42 +8,84 @@ using System.Threading.Tasks;
 
 namespace Curd_Procedure_AddressBook_ADO
 {
-    public class AddressBook
-    {
-        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook;Integrated Security=true;";
-        public void AddNewContactInDatabase(Contact contact)
+        public class AddressBook
         {
-            SqlConnection sqlconnection = new SqlConnection(connectionString);
-            try
+            public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AddressBook;Integrated Security=true;";
+            public void AddNewContactInDatabase(Contact contact)
             {
-                using (sqlconnection)
+                SqlConnection sqlconnection = new SqlConnection(connectionString);
+                try
                 {
-                    sqlconnection.Open();
-                    SqlCommand command = new SqlCommand("SPAddingNewData", sqlconnection);
-                    command.CommandType = CommandType.StoredProcedure;
-                    command.Parameters.AddWithValue("@FirstName", contact.FirstName);
-                    command.Parameters.AddWithValue("@LastName", contact.LastName);
-                    command.Parameters.AddWithValue("@Address", contact.Address);
-                    command.Parameters.AddWithValue("@City", contact.City);
-                    command.Parameters.AddWithValue("@State", contact.State);
-                    command.Parameters.AddWithValue("@ZIP", contact.Zip);
-                    command.Parameters.AddWithValue("@PhoneNumber", contact.PhoneNUmber);
-                    command.Parameters.AddWithValue("@Email", contact.Email);
-                    int result = command.ExecuteNonQuery();
-                    sqlconnection.Close();
-                    if (result >= 1)
+                    using (sqlconnection)
                     {
-                        Console.WriteLine("New Contact Added Successfully");
+                        sqlconnection.Open();
+                        SqlCommand command = new SqlCommand("SPAddingNewData", sqlconnection);
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@FirstName", contact.FirstName);
+                        command.Parameters.AddWithValue("@LastName", contact.LastName);
+                        command.Parameters.AddWithValue("@Address", contact.Address);
+                        command.Parameters.AddWithValue("@City", contact.City);
+                        command.Parameters.AddWithValue("@State", contact.State);
+                        command.Parameters.AddWithValue("@ZIP", contact.Zip);
+                        command.Parameters.AddWithValue("@PhoneNumber", contact.PhoneNUmber);
+                        command.Parameters.AddWithValue("@Email", contact.Email);
+                        int result = command.ExecuteNonQuery();
+                        sqlconnection.Close();
+                        if (result >= 1)
+                        {
+                            Console.WriteLine("New Contact Added Successfully");
+                        }
+                        else
+                            Console.WriteLine("No DataBase found");
                     }
-                    else
-                        Console.WriteLine("No DataBase found");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
-            catch (Exception ex)
+            public void RetrieveEntriesFromAddressBookDB()
             {
-                // handle exception here
-                Console.WriteLine(ex.Message);
+                SqlConnection sqlconnection = new SqlConnection(connectionString);
+                try
+                {
+                    List<Contact> contact = new List<Contact>();
+                    using (sqlconnection)
+                    {
+                        sqlconnection.Open();
+                        SqlCommand command = new SqlCommand("SPRetrieveAllDetails", sqlconnection);
+                        command.CommandType = CommandType.StoredProcedure;
+                        SqlDataReader dr = command.ExecuteReader();
+                        if (dr.HasRows)
+                        {
+                            while (dr.Read())
+                            {
+                                Contact contactNew = new Contact();
+                                contactNew.FirstName = dr.GetString(0);
+                                contactNew.LastName = dr.GetString(1);
+                                contactNew.Address = dr.GetString(2);
+                                contactNew.City = dr.GetString(3);
+                                contactNew.State = dr.GetString(4);
+                                contactNew.Zip = dr.GetInt32(5);
+                                contactNew.PhoneNUmber = dr.GetInt64(6);
+                                contactNew.Email = dr.GetString(7);
+                                contact.Add(contactNew);
+                            }
+                            foreach (var data in contact)
+                            {
+                                Console.WriteLine("Firstame:" + data.FirstName + "  " + "LastName:" + data.LastName + "  " + "Address:" + data.Address + "  " + "City:" + data.City + "  " + "State:" + data.State + "  " + "ZIP:" + data.Zip + "  " + "PhoneNo:" + data.PhoneNUmber + "  " + "Email:" + data.Email);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No Database Found");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
     }
-}
